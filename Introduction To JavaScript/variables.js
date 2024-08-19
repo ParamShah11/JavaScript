9a1ed5cadf46659556fc5901c93a2f00081d5fc8
ParamShah11 <shahparam118@gmail.com>
2024-08-19 04:42:22 +0000

    Variables In Js

---
+++ Introduction To JavaScript/variables.js
@@ -0,0 +1,11 @@
+const accountId = 123456
+let accountName = "Hello World"
+var accountPassword = 567;
+accountCity = "Ahmedabad"
+
+/*
+    prefer not to use var because it's not 
+    block-scoped.
+*/
+
+console.table([accountId,accountName,accountPassword,accountCity])
